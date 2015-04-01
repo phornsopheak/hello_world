3c5912c0fe65f1dedcfc53c9feb6936619e0662f
Phorn Sopheak <phornsopheak17@gmail.com>
2015-04-01 23:31:46 +0700

    Change some point

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,5 @@
 Rails.application.routes.draw do
   
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
- 
 root 'application#hello'
 
 end
